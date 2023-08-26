4bfd82528e6497fe90cc8630e2a8de717477bdbc
minliacom <minliacom@gmail.com>
2023-08-26 18:54:43 +0800

    feat: update commitlint settings

--- apps/nextjs/src/app/api/hello/route.ts
+++ apps/nextjs/src/app/api/hello/route.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 const handler = (_req: NextRequest) => {
   const result = { message: "hi" };
   console.log(`Api hello response body ${JSON.stringify(result)}`);
-  console.log(`For husky and lint-staged test only`);
+  console.log(`For husky and lint-staged test only 123`);
   return new NextResponse(JSON.stringify(result), {
     headers: {
       "Content-Type": "application/json;charset=UTF-8",
