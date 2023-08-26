7aed23f1685b05826b890dd1ce5ddc92eb2d1973
minliacom <minliacom@gmail.com>
2023-08-27 03:58:43 +0800

    feat: update code format

---
+++ packages/db/src/index.ts
@@ -0,0 +1,7 @@
+import { customAlphabet } from "nanoid";
+
+// export * from "./prisma/enums";
+export * from "./lib";
+export { Prisma } from "@prisma/client";
+
+export const genId = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 16);
