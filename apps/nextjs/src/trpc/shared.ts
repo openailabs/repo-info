dc43876e504e021a2e13212cf7ff4641c53bb0a4
minliacom <minliacom@gmail.com>
2023-08-29 17:18:06 +0800

    feat: change default port to 3000

--- apps/nextjs/src/trpc/shared.ts
+++ apps/nextjs/src/trpc/shared.ts
@@ -9,7 +9,7 @@ const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
   const vc = process.env.VERCEL_URL;
   if (vc) return `https://${vc}`;
-  return `http://localhost:2000`;
+  return `${process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"}`;
 };
 
 const lambdas = ["stripe", "ingestion"];
--- packages/api/src/env.mjs
+++ packages/api/src/env.mjs
@@ -4,7 +4,7 @@ import * as z from "zod";
 export const env = createEnv({
   shared: {},
   server: {
-    NEXTJS_URL: z.preprocess(
+    NEXT_PUBLIC_APP_URL: z.preprocess(
       (str) =>
         process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : str,
       process.env.VERCEL_URL ? z.string().min(1) : z.string().url(),
